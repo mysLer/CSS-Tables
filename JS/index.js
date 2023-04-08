2c9c85428109002e865e0bd4fa4aa4a6f027eb41
mnd <mehmetndincer@gmail.com>
2023-04-08 14:44:23 +0200

    Javascript1

---
+++ JS/index.js
@@ -0,0 +1,29 @@
+console.log("Hello World!")
+
+var userName = "Gamze"
+
+userName = "Ebuzer"
+
+console.log(userName)
+
+let mail ="abc@gmail.com"
+
+mail="xyz" 
+
+console.log(mail)
+
+//let -> mutable (degistirilebilir degisken, yani tekrar let ile baslayan bir mail yamamam)
+
+console.log(mail)
+
+mail = "xyz@gmail.com"
+
+console.log(mail)
+
+const email = "ababab"
+
+//const -> immutable (degistirilemez degisken)
+
+let uName;
+
+//const secondName;   ( yani bu sekilde hata verir, const un yanina yeni bir veri atayamassin)
